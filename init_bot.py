6c8eaa356d6509b8f678b8b0f2d80d475daea3dc
unknown <sairus.89@mail.ru>
2026-02-03 01:54:30 +0300

    update requirements

--- init_bot.py
+++ init_bot.py
@@ -1,7 +1,29 @@
+# import os
+# from aiogram import Bot, Dispatcher
+# from dotenv import load_dotenv
+
+# load_dotenv()
+# bot = Bot(token=os.getenv('BOT-TOKEN'))
+# dp = Dispatcher()
+
 import os
+try:
+    from google.colab import userdata
+except ImportError:
+    userdata = None
+
 from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
 
 load_dotenv()
-bot = Bot(token=os.getenv('BOT-TOKEN'))
+
+if userdata and 'BOT_TOKEN' in userdata:
+    token = userdata.get('BOT_TOKEN')
+else:
+    token = os.getenv('BOT_TOKEN')
+
+if not token:
+    raise ValueError("Токен бота не найден. Проверьте Colab Secrets или ваш .env файл.")
+
+bot = Bot(token=token)
 dp = Dispatcher()
